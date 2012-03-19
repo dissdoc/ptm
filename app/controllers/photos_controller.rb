8ab828c002abd24e2a28b4ba9f8c1c9938a603e0
Kirill Muchow <dissdoc@gmail.com>
2012-03-19 22:27:44 +0300

    add photo to album

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,18 @@
 class PhotosController < ApplicationController
+  before_filter :authenticate_user!
 
   def new
     @album = Album.find(params[:album_id])
     @photo = @album.photos.new
+  end
+
+  def create
+    @album = Album.find(params[:album_id])
+    @photo = @album.photos.new(params[:photo])
+    if @photo.save!
+      redirect_to @album
+    else
+      redirect_to root_path
+    end
   end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,7 +2,7 @@ class Photo < ActiveRecord::Base
   belongs_to :album
 
   has_attached_file :image,
-                    :styles => {:icon => "64x64>", :small => "100x63>", :medium => '260x180>', :large => "483x302>" }
+                    :styles => {:icon => "64x64>", :small => "100x63>", :medium => "260x180>", :large => "483x302>" }
 
   attr_accessible :album_id, :image
 end
