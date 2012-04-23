7c69fc861054da8abcf74e362060dbe7f7bf539c
Kirill Muchow <dissdoc@gmail.com>
2012-04-23 20:35:50 +0400

    add configure for groups and group_joins

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,25 @@
 class Group < ActiveRecord::Base
+  has_many :group_joins, :dependent => :destroy
+
+  has_many :admins,
+      :through => :group_joins,
+      :class_name => "User",
+      :source => :user,
+      :conditions => ['group_joins.role = ?', 'admin']
+
+  has_many :members,
+      :through => :group_joins,
+      :class_name => "User",
+      :source => :user,
+      :conditions => ['group_joins.role = ? AND group_joins.accepted = ?', 'member', true]
+
+  has_many :candidates,
+      :through => :group_joins,
+      :class_name => "User",
+      :source => :user,
+      :conditions => ['group_joins.role = ? AND group_joins.accepted = ?', 'member', false]
+
+  attr_accessible :name, :description
+
+  validates :name
 end
--- app/models/group_join.rb
+++ app/models/group_join.rb
@@ -1,2 +1,16 @@
 class GroupJoin < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :group
+
+  validates :user_id, :group_id, :role, :presence => true
+  validates :user_id, :uniqueness => { :scope => :group_id, :message => "should happen once per club" }
+  validates :role, :inclusion => { :in => %w(admin member), :message => "Role %{value} is reserved" }
+
+  def accepted!
+    update_attribute(:accepted, true)
+  end
+
+  def reject!
+    update_attribute(:accepted, false)
+  end
 end
