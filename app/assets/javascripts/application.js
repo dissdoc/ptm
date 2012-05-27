9a06d035c54b6316f987c7ffc5ddd896a1d068f7
Kirill Muchow <dissdoc@gmail.com>
2012-05-27 22:54:24 +0400

    my idea

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,17 @@
 //= require_tree .
 
 $(document).ready(function(){
+    // create map
+    initialize();
+//    var latlng = new google.maps.LatLng(-34.397, 150.644);
+//    var myOptions = {
+//        zoom: 8,
+//        center: latlng,
+//        mapTypeId: google.maps.MapTypeId.ROADMAP
+//    };
+//    var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+    // end of create map
+
     $('#dropdown li.headlink').hover(
         function() { $('ul', this).css('display', 'block'); },
         function() { $('ul', this).css('display', 'none'); });
@@ -34,3 +45,40 @@ $(document).ready(function(){
         return false;
     });
 });
+
+function initialize() {
+    this.map = null;
+    this.geocoder = null;
+
+    if (this.geocoder == null)
+        this.geocoder = new google.maps.Geocoder();
+
+    var lat = $('#lat').attr('value');
+    var lng = $('#lng').attr('value');
+    var latlng = new google.maps.LatLng(lat, lng);
+    var myOptions = {
+        zoom: 8,
+        center: latlng,
+        mapTypeId:google.maps.MapTypeId.ROADMAP
+    }
+    this.map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+}
+
+function getZoom() {
+    alert("thats all");
+}
+
+function setMapToCity(map, geocoder) {
+    var address = $('#address').attr('value');
+    geocoder.geocode({ 'address': address }, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            map.setCenter(results[0].geometry.location);
+            //установить Zoom таким образом, чтобы город был показан весь
+            map.setZoom(_this.getZoom(results[0].geometry.viewport));
+            //и поставить маркет для отметки адреса
+            addMarker();
+        } else {
+            alert("Пошло что-то не так, потому что: " + status);
+        }
+    });
+}
