f0eac94eae13a6788f869eb146b3bf113409d5cb
Kirill Mukhov <dissdoc@gmail.com>
2012-06-25 16:02:53 +0400

    fix template

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,8 @@ class UserMailer < ActionMailer::Base
   default :from => 'hello@phototimemachine.org'
 
   def added_photo_favorite(user, photo)
+    @user = user
+    @photo = photo
     mail(
         :to => photo.user.email,
         :subject => "#{user.full_name} added your photo '#{photo.comment}' as a favorite"
