089eb8e2074e97cda680d140bbef6be0d6b5903b
Kirill Mukhov <dissdoc@gmail.com>
2012-06-29 15:50:56 +0400

    search by map

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,49 +2,27 @@ class HomeController < ApplicationController
   before_filter :authenticate_user!, :only => [:profile]
 
   def index
-    #if params[:ne_lat] && params[:ne_lng] && params[:lat] && params[:lng]
-    #  max_lat = params[:ne_lat].to_f
-    #  max_lng = params[:ne_lng].to_f
-    #  min_lat = params[:sw_lat].to_f
-    #  min_lng = params[:sw_lng].to_f
-    #
-    #  max_lat, min_lat = min_lat, max_lat if max_lat < min_lat
-    #  max_lng, min_lng = min_lng, max_lng if max_lng < min_lng
-    #
-    #  @locations = Geo.where('latitude <= ? AND latitude >= ? AND longitude <= ? AND longitude >= ?', max_lat, min_lat, max_lng, min_lng)
-    #  @photos = @locations.collect(&:photo)
-    #else
-    #  @photos = Photo.all
-    #end
-    #
-    #respond_to do |format|
-    #  format.js { render :template => 'photos/refresh.js.erb' }
-    #end
-    #
-    #if params[:search_locations].present?
-    #  @locations = Geo.near(params[:search_locations])
-    #  @photos = @locations.collect(&:photo)
-    #elsif params[:search_tags].present?
-    #  @tag = Tag.get(params[:search_tags])
-    #  @taggings = Tagging.get_by_tag(@tag)
-    #  @photos = []
-    #  @taggings.each do |tagging|
-    #    @photos << tagging.photo
-    #  end
-    #elsif params[:search_dt].present?
-    #  @photos = Photo.get_by_datetime(params[:search_dt].to_s)
-    #else
-    #  @photos = Photo.all(:joins => :share_photo, :conditions => {:share_photos => {:share => true}})
-    #end
-
     if params[:search_tags].present?
       list_tag = params[:search_tags].split(/,\s*/)
       tags = Tag.find(:all, :conditions => ["name IN (?)", list_tag]).collect(&:id).uniq
       ids = Tagging.find(:all, :conditions => ["tag_id IN (?)", tags]).collect(&:photo)
       @photos = Photo.find(:all, :conditions => ["id IN (?)", ids]).uniq
-    else
+    end
+
+    if params[:ne_lat].present?
+      photos_geo = Geo.get_all(params[:ne_lat].to_f, params[:ne_lng].to_f, params[:sw_lat].to_f, params[:sw_lng].to_f).collect(&:photo).uniq
+      if @photos.present? && @photos.count > 0
+        @photos = @photos & photos_geo
+      else
+        @photos = photos_geo
+      end
+    end
+
+    if @photos.blank?
       @photos = Photo.all
     end
+
+    @photos_for_map = Photo.all
 
     render :layout => 'application_empty'
   end
--- app/models/geo.rb
+++ app/models/geo.rb
@@ -5,4 +5,11 @@ class Geo < ActiveRecord::Base
   geocoded_by :address
 
   after_validation :geocode, :if => :address_changed?
+
+  def self.get_all(max_lat, max_lng, min_lat, min_lng)
+    max_lat, min_lat = min_lat, max_lat if max_lat < min_lat
+    max_lng, min_lng = min_lng, max_lng if max_lng < min_lng
+
+    where('latitude <= ? AND latitude >= ? AND longitude <= ? AND longitude >= ?', max_lat, min_lat, max_lng, min_lng)
+  end
 end
