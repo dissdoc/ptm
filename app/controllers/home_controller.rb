ffbddf4052bf4ef4ce268c89c36d94570a6a5426
Kirill Mukhov <dissdoc@gmail.com>
2012-07-20 15:38:12 +0400

    bug fix search by map

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,19 +2,23 @@ class HomeController < ApplicationController
   before_filter :authenticate_user!, :only => [:profile]
 
   def index
-    if params[:search_tags].present?
-      list_tag = params[:search_tags].split(/,\s*/)
-      tags = Tag.all(:conditions => ["name IN (?)", list_tag]).collect(&:id).uniq
-      ids = Tagging.all(:conditions => ["tag_id IN (?)", tags]).collect(&:photo)
-      @photos = Photo.all(:conditions => ["id IN (?)", ids]).uniq
-    end
+    # if params[:search_tags].present?
+    #   list_tag = params[:search_tags].split(/,\s*/)
+    #   tags = Tag.all(:conditions => ["name IN (?)", list_tag]).collect(&:id).uniq
+    #   ids = Tagging.all(:conditions => ["tag_id IN (?)", tags]).collect(&:photo)
+    #   @photos = Photo.all(:conditions => ["id IN (?)", ids]).uniq
+    # end
 
     if params[:ne_lat].present?
-      distance = Geocoder::Calculations.distance_between([params[:ne_lat].to_f, params[:ne_lng].to_f],
-                                                         [params[:sw_lat].to_f, params[:sw_lng].to_f])
-      center = Geocoder::Calculations.geographic_center([[params[:ne_lat].to_f, params[:ne_lng].to_f],
-                                                        [params[:sw_lat].to_f, params[:sw_lng].to_f]])
-      photos_geo = Geo.near(center, distance/2).collect(&:photo)
+      geos = Geo.where('latitude < ? AND latitude > ? AND longitude < ? AND longitude > ?', 
+                params[:ne_lat], params[:sw_lat],
+                params[:ne_lng], params[:sw_lng])
+      photos_geo = geos.collect(&:photo)
+      # distance = Geocoder::Calculations.distance_between([params[:ne_lat].to_f, params[:ne_lng].to_f],
+                                                         # [params[:sw_lat].to_f, params[:sw_lng].to_f])
+      # center = Geocoder::Calculations.geographic_center([[params[:ne_lat].to_f, params[:ne_lng].to_f],
+      #                                                   [params[:sw_lat].to_f, params[:sw_lng].to_f]])
+      # photos_geo = Geo.near(center, distance/2).collect(&:photo)
       if @photos.present? && @photos.count > 0
         @photos = @photos & photos_geo
       else
@@ -22,23 +26,23 @@ class HomeController < ApplicationController
       end
     end
 
-    minD = params[:minD].to_i
-    maxD = params[:maxD].to_i
-    minD = 5 if minD < 5
-    maxD = 176 if maxD > 176
+    # minD = params[:minD].to_i
+    # maxD = params[:maxD].to_i
+    # minD = 5 if minD < 5
+    # maxD = 176 if maxD > 176
 
-    min_date = minD - 5 + 1826
-    max_date = maxD - 5 + 1826
+    # min_date = minD - 5 + 1826
+    # max_date = maxD - 5 + 1826
 
-    start = DateTime.civil(min_date)
-    fin = DateTime.civil(max_date)
+    # start = DateTime.civil(min_date)
+    # fin = DateTime.civil(max_date)
 
-    photos_date = Photo.where('generate > ? AND generate < ?', start, fin)
-    if @photos.present? && @photos.count > 0
-      @photos = @photos & photos_date
-    else
-      @photos = photos_date
-    end
+    # photos_date = Photo.where('generate > ? AND generate < ?', start, fin)
+    # if @photos.present? && @photos.count > 0
+    #   @photos = @photos & photos_date
+    # else
+    #   @photos = photos_date
+    # end
 
     if @photos.blank?
       @photos = Photo.all
