05438f7d61d128d94d340e8b9996fe3baca5cd25
Kirill Muchow <dissdoc@gmail.com>
2012-07-28 22:04:12 +0400

    add skeleton for ptm.geolocation plugin

---
+++ app/assets/javascripts/ptm.geolocation.js
@@ -0,0 +1,10 @@
+(function($) {
+    $.fn.ptmGeolocation = function(options) {
+        var defaults = {
+            width: '320px',
+            height_map: '240px'
+        }, 	opts = $.extend(defaults, options);
+
+        // get value ====> opts['location']
+    };
+})(jQuery);
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Ptm::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.assets.precompile += %w( style.css ptm.search.css ptm.search.js )
+  config.assets.precompile += %w( style.css ptm.search.css ptm.search.js ptm.geolocation.css ptm.geolocation.js)
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
