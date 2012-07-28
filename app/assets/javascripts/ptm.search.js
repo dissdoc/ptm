708d7a25f8914f0783812ef89962a66cf8fe038a
Kirill Muchow <dissdoc@gmail.com>
2012-07-28 16:44:25 +0400

    add dropmenu for search field. End of plugin search field

--- app/assets/javascripts/ptm.search.js
+++ app/assets/javascripts/ptm.search.js
@@ -1,24 +1,46 @@
 (function($){
     $.fn.ptmSearch = function(options) {
-        var defaults = {
-                cl: 'inactive',
-                text: this.val()
-            }, opts = $.extend(defaults, options);
+        var ul = this.find('.combo ul');
+        this.find('#search_value').val('1');
 
-        this.addClass(opts['cl']);
-        this.val(opts['text']);
+        // Prepare for ul position
+        var comboPos = this.find('.combo').position();
+        // minus and plus 1 - for border 1px
+        ul.css( {'top': comboPos.top + 30 + 1, 'left': ul.offset().left - 90 - 1 })
+        ul.hide();
 
-        this.focus(function() {
-           if ($(this).val() == opts['text'])
-               $(this).val('');
-            $(this).removeClass(opts['cl']);
+        this.find('.combo div').hover(
+            function() {
+                $(this).css({'cursor': 'pointer'});
+                $(this).find('p').css({'color': '#999'});
+            },
+            function() {
+                $(this).find('p').css({'color': '#666'});
+            }
+        );
+
+        this.find('.combo div').click(function() {
+            if (!ul.is(':visible'))
+                ul.animate({opacity: 'show'}, 'fast');
+            else
+                ul.animate({opacity: 'hide'}, 'fast');
         });
 
-        this.blur(function() {
-            if ($(this).val() == '') {
-                $(this).addClass(opts['cl']);
-                $(this).val(opts['text']);
-            }
+        this.find('.combo ul li').mouseover(function() {
+            $(this).css({'background':'#eee', 'cursor': 'pointer'});
+        });
+
+        this.find('.combo ul li').mouseout(function() {
+           $(this).css({'background': '#fff'});
+        });
+
+        this.find('.combo ul li').click(function() {
+            $('#search_value').val( $(this).find('a').attr('value'));
+            ul.animate({opacity: 'hide'}, 'fast');
+        });
+
+        $(this).mouseleave(function(){
+            ul.animate({opacity: 'hide'}, 'fast');
         });
     };
 })(jQuery);
